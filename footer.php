0ba8d278f909ab9d7a41acc8a512c3be42beb550
moinuddin17 <moinuddinmd38@gmail.com>
2025-03-28 16:41:00 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,95 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 mb-4 mb-lg-0">
+                <div class="footer-logo">
+                    <a href="#">
+                        <img src="img/logo.png" alt="Awesome Logo">
+                    </a>
+                </div>
+                <div class="copyright mt-4">
+                    <p>© 2020 <span>Jevelin</span> All Rights Reserved.</p>
+                </div>
+                <div class="social-links">
+                    <ul class="d-flex gap-4 p-0 m-0 list-unstyled">
+                        <li>
+                            <a href="#" target="_blank">
+                                <svg width="13" height="14" viewBox="0 0 13 12" fill="#000" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_1027_3715)">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M12.1072 11.8918H9.44938V8.04862C9.44938 7.04287 9.03395 6.35607 8.11949 6.35607C7.42039 6.35607 7.03149 6.81959 6.85056 7.26617C6.78251 7.42632 6.79318 7.64961 6.79318 7.87289V11.8918H4.1602C4.1602 11.8918 4.19409 5.08414 4.1602 4.46539H6.79318V5.63101C6.94896 5.12131 7.79018 4.39406 9.13262 4.39406C10.7984 4.39406 12.1072 5.46211 12.1072 7.76247V11.8918ZM1.49772 3.53645H1.48051C0.632184 3.53645 0.0820312 2.9688 0.0820312 2.2492C0.0820312 1.51567 0.648311 0.959778 1.51385 0.959778C2.37857 0.959778 2.9104 1.5143 2.92707 2.24702C2.92707 2.96661 2.37856 3.53645 1.49772 3.53645ZM0.38539 4.46539H2.7292V11.8918H0.38539V4.46539Z" fill="white"></path>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_1027_3715">
+                                            <rect width="12.0252" height="10.932" fill="white" transform="translate(0.0820312 0.960083)"></rect>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="#000" class="bi bi-twitter" viewBox="0 0 16 16">
+                                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <svg width="14" height="14" viewBox="0 0 7 13" fill="#000" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M4.34122 12.8631V7.37916H6.16764L6.44108 5.24196H4.34122V3.87742C4.34122 3.25866 4.51173 2.83695 5.39219 2.83695L6.5151 2.83642V0.924943C6.3208 0.898992 5.65428 0.84082 4.87883 0.84082C3.25985 0.84082 2.15145 1.83676 2.15145 3.66586V5.24201H0.320312V7.37921H2.15139V12.8632L4.34122 12.8631Z" fill="white"></path>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="title">
+                    <h3>Services</h3>
+                </div>
+                <div class="usefull-links">
+                    <ul>
+                        <li><a href="#">Financing</a></li>
+                        <li><a href="#">Accounting</a></li>
+                        <li><a href="#">Something</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="title">
+                    <h3>About Us</h3>
+                </div>
+                <div class="usefull-links">
+                    <ul>
+                        <li><a href="#">Vision</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Reviews</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="contact-title">
+                    <h3>Contact</h3>
+                </div>
+                <div class="contact-links">
+                    <p>jevelin@emailname.com</p>
+                    <p>+731 442 522 96</p>
+                    <p>Mon.-Fr. 9AM-5PM</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<button class="scroll-top active" data-scroll="up" type="button">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#cecece" class="bi bi-chevron-up" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+    </svg>
+</button>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,259 @@
+<?php include 'header.php' ?>
+<!-- App Section Start -->
+<section class="appSection aos-init" data-aos="fade-up" data-aos-duration="500" id="appSection" style="background-image:url(img/main-bg.jpg); background-size:cover;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="app-content">
+                    <h3>New app on the block</h3>
+                    <h2>Jevelin app will never again let you worry about your plants</h2>
+                    <div class="">
+                        <a href="#" class="app-btn mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                            </svg>
+                            <span>Find out more</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="main-img">
+                    <img src="img/main-img.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- App Section End -->
+<!-- Features Section Start -->
+<section class="featuresSection aos-init" data-aos="fade-up" data-aos-duration="500" id="featuresSection">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="features-cont text-center">
+                    <h3>Features</h3>
+                    <h2>Watch our quick <br>introduction video</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate quam ut mi dictum, et euismod sapien condimentum. Etiam a ante at nunc mattis sagittis ut ac leo. Etiam turpis augue, laoreet et velit vitae, pulvinar rhoncus massa. Curabitur efficitur commodo euismod.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Features Section End -->
+<!-- Testimonial Section Start -->
+<section class="testimonialSection" id="testimonialSection" style="background-image:url(img/testimonial-bg.jpg);">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="swiper mySwiper">
+                    <div class="swiper-wrapper pb-5">
+                        <div class="swiper-slide">
+                            <div class="testimonial-card text-center">
+                                <div>
+                                    <img src="img/jane.jpg" class="img-fluid rounded-circle" alt="">
+                                </div>
+                                <h2>Jane Doe</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate quam ut mi dictum, et euismod sapien condimentum. Etiam a ante at nunc mattis</p>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="testimonial-card text-center">
+                                <div>
+                                    <img src="img/harley.jpg" class="img-fluid rounded-circle" alt="">
+                                </div>
+                                <h2>Harley Doe</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate quam ut mi dictum, et euismod sapien condimentum. Etiam a ante at nunc mattis</p>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="testimonial-card text-center">
+                                <div>
+                                    <img src="img/john.jpg" class="img-fluid rounded-circle" alt="">
+                                </div>
+                                <h2>John Doe</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate quam ut mi dictum, et euismod sapien condimentum. Etiam a ante at nunc mattis</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="swiper-pagination"></div> -->
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="d-flex justify-content-center">
+                    <div class="play-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                        <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Testimonial Section End -->
+<!-- Endless Section Start -->
+<section class="endlessSection" id="endlessSection">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="easyCare-img">
+                    <img src="img/plant.png" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="main-content aos-init" data-aos="fade-left" data-aos-duration="500">
+                    <h3>More than 10k plants</h3>
+                    <h2>Endless plant library to choose from</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate quam ut mi dictum, et euismod sapien condimentum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Endless Section End -->
+<!-- Planner Section Start -->
+<section class="plannerSection mt-4 mt-lg-0" id="plannerSection">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6  aos-init" data-aos="fade-right" data-aos-duration="500">
+                <div class="main-content">
+                    <h3>Advanced planner</h3>
+                    <h2>Schedule your plants needs</h2>
+                </div>
+                <div>
+                    <div class="title">
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#01d679" class="bi bi-bell" viewBox="0 0 16 16">
+                                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+                            </svg>
+                        </div>
+                        <div class="text">
+                            <h3>Get notified when your plants cycles are starting</h3>
+                        </div>
+                    </div>
+                    <div class="title mt-5">
+                        <div class="icon icon2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#ffa813" class="bi bi-calendar" viewBox="0 0 16 16">
+                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+                            </svg>
+                        </div>
+                        <div class="text">
+                            <h3>Set separate reminders for different seasons</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="planner-img text-end">
+                    <img src="img/schedule.png" class="img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Planner Section End -->
+<!-- Notified Section Start -->
+<section class="notifiedSection" id="notifiedSection">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="notified-img">
+                    <img src="img/never.png" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="main-content aos-init" data-aos="fade-left" data-aos-duration="500">
+                    <h3>Never forget</h3>
+                    <h2>Get notified whenever you need to attend your plants</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vulputate quam ut mi dictum, et euismod sapien condimentum.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Notified Section End -->
+<!-- Pricing Section Start -->
+<section class="pricingSection" id="pricingSection" style="background-image:url(img/pricing.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <div class="pricing-content">
+                    <h3 class="mb-4">Pricing</h3>
+                    <h2>Choose what suits your needs the best</h2>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="price-card text-center aos-init" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-offset="0">
+                    <h3>Basic plan</h3>
+                    <h2>Free</h2>
+                    <h4>/monthly</h4>
+                    <div class="mb-5">
+                        <a href="#" class="basic-btn mt-4">
+                            <span>Get started</span>
+                        </a>
+                    </div>
+                    <p>Unlimited plant library</p>
+                    <p>Calendar</p>
+                    <p>Schedule alerts</p>
+                    <p>Up to 10 plants</p>
+                    <p>-</p>
+                </div>
+            </div>
+            <div class="col-lg-4 mt-5 mt-lg-0">
+                <div class="price-card text-center aos-init" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-offset="0">
+                    <h3>Premium plan</h3>
+                    <h2 class="premium">$10</h2>
+                    <h4>/monthly</h4>
+                    <div class="mb-5">
+                        <a href="#" class="premium-btn mt-4">
+                            <span>Get started</span>
+                        </a>
+                    </div>
+                    <p>Unlimited plant library</p>
+                    <p>Calendar</p>
+                    <p>Schedule alerts</p>
+                    <p>Up to 100 plants</p>
+                    <p>20GB gallery</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Pricing Section End -->
+<!-- Contact Section Start -->
+<section class="contactSection" id="contactSection" style="background-image:url(img/contact-bg.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="reachus-content text-center">
+                    <h3>Reach us</h3>
+                    <h2>Do not hesitate to ask <br>us any questions</h2>
+                    <div class="">
+                        <a href="#" class="theme-btn mt-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="#fff" class="bi bi-chat-left" viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            </svg>
+                            <span>Contact us</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Contact Section End -->
+<?php include 'footer.php' ?>
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+            clickable: true,
+        },
+    });
+</script>
+<script>
+    AOS.init();
+</script>
